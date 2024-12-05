a74004424ec1237db734c66c3aa129bff2bc5d13
Nate Johnson <152443147+crispy-cths@users.noreply.github.com>
2024-12-05 13:38:56 -0700

    Update Microsoft.PowerShell_profile.ps1

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,6 +1,6 @@
 $PSVersionTable.PSEdition -ne 'Core' | Out-Null
 
-oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\catppuccin.omp.json" | Invoke-Expression
+oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\catppuccin_mocha.omp.json" | Invoke-Expression
 
 # terminal icons
 Import-Module -Name Terminal-Icons
